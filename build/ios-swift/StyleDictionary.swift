a7bf5945ed3265b8c4b957ee6878bf982a936986
jussijooWorking <jussijooWorking@users.noreply.github.com>
2022-09-28 09:27:53 +0000

    Tokens updated

--- build/ios-swift/StyleDictionary.swift
+++ build/ios-swift/StyleDictionary.swift
@@ -4,16 +4,17 @@
 //
 
 // Do not edit directly
-// Generated on Mon, 26 Sep 2022 13:18:55 GMT
+// Generated on Wed, 28 Sep 2022 09:27:53 GMT
 
 
 import UIKit
 
 public class StyleDictionary {
     public static let colorColorColorsGree = UIColor(red: 0.769, green: 0.816, blue: 0.804, alpha: 1)
-    public static let colorColorColorsGroon = UIColor(red: 0.086, green: 0.882, blue: 0.690, alpha: 1)
+    public static let colorColorColorsGroon = UIColor(red: 0.145, green: 0.820, blue: 0.659, alpha: 1)
     public static let fontFontTextBody = "[object Object]"
     public static let fontFontTextHeading1 = "[object Object]"
+    public static let spacingSpacing20 = [object Object]
     public static let typographyTypographyTextBodyFontFamily = Roboto
     public static let typographyTypographyTextBodyFontSize = 16
     public static let typographyTypographyTextBodyFontStretch = normal
@@ -26,12 +27,12 @@ public class StyleDictionary {
     public static let typographyTypographyTextBodyTextCase = none
     public static let typographyTypographyTextBodyTextDecoration = none
     public static let typographyTypographyTextHeading1FontFamily = Roboto
-    public static let typographyTypographyTextHeading1FontSize = 32
+    public static let typographyTypographyTextHeading1FontSize = 48
     public static let typographyTypographyTextHeading1FontStretch = normal
     public static let typographyTypographyTextHeading1FontStyle = normal
     public static let typographyTypographyTextHeading1FontWeight = 300
     public static let typographyTypographyTextHeading1LetterSpacing = 0
-    public static let typographyTypographyTextHeading1LineHeight = 38.4
+    public static let typographyTypographyTextHeading1LineHeight = 57.6
     public static let typographyTypographyTextHeading1ParagraphIndent = 0
     public static let typographyTypographyTextHeading1ParagraphSpacing = 0
     public static let typographyTypographyTextHeading1TextCase = none
